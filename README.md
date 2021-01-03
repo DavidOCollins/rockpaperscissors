42f54b674994a7a69218ed3c9a74a2ed8892e77c
SetLikeJelly <davidcollins2220@gmail.com>
2021-01-03 18:55:18 +0100

    updated, with pointers

---
+++ README.md
@@ -0,0 +1,3 @@
+Odin project/Freecodecamp tutorial
+
+-- added images, worked with the DOM, past what odin suggested
---
+++ app.js
@@ -0,0 +1,7 @@
+//set up references to the html eg score/hand chosen
+
+//the computer chooses randomly from 3 choices
+
+//use can choose and touch events
+
+//game round- compare value of one to other, winners and losers
